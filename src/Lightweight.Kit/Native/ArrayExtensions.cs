a55c9794bd97937fd6cb0a8f3b045d71b75d24ca
Viktor <mykhaylyshynorman@gmail.com>
2022-09-30 15:11:38 +0300

    Add array exstensions

---
+++ src/Lightweight.Kit/Native/ArrayExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lightweight.Kit.Native
+{
+    public static class ArrayExtensions
+    {
+        public static bool IsNullOrEmpty(this Array array)
+        {
+            return array == null || array.Length == 0;
+        }
+
+        public static bool NotNullOrEmpty(this Array array)
+        {
+            return IsNullOrEmpty(array) == false;
+        }
+
+        public static T[] OrEmpty<T>(this T[] array)
+        {
+            return array ?? Array.Empty<T>();
+        }
+    }
+}
